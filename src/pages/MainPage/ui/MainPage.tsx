53f56a1694af2b8f27ad75ef1f0e96fdde909721
ArizaTrizniak <arizatrizniak@gmail.com>
2023-12-13 14:45:04 -0600

    2.17

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {t} from "i18next";
+import {useTranslation} from "react-i18next";
+
+const MainPage = () => {
+    const {t} =useTranslation();
+    return (
+        <div>
+            {t('Главная')}
+        </div>
+    );
+};
+
+export default MainPage;
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import AppLink, {AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+
+interface NavbarProps {
+    className?: string;
+}
+
+const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.Links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={'/'}
+                    className={cls.MainLink}
+                >
+                    Главная
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={'/about'}
+                >
+                    О сайте
+                </AppLink>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
