9ea54b576f58878d93afc05f4963ae172ff0d0ba
ArizaTrizniak <arizatrizniak@gmail.com>
2024-01-02 09:43:29 -0600

    add page loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,10 +2,11 @@ import React, {Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {routeConfig} from 'shared/config/routeConfig/routeConfig';
 import {t} from 'i18next';
+import PageLoader from 'widgets/PageLoader/ui/PageLoader';
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>{t('Загрузка...')}</div>}>
+        <Suspense fallback={<PageLoader />}>
             <Routes>
                 {Object.values(routeConfig).map(({element, path}) => (
                     <Route
