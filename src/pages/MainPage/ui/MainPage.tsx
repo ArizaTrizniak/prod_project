ded0c8b4d3491672ec9a8f63bff331d0f51540e3
ArizaTrizniak <arizatrizniak@gmail.com>
2024-03-03 14:46:19 -0600

    add new input, add login form

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import {useTranslation} from 'react-i18next';
-import {Counter} from 'entities/Counter';
-import {Input} from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const {t} =useTranslation();
-    const [value, setValue] = useState('');
 
-    const onChange = ( val:string) => {
-        setValue(val);
-    };
     return (
         <div>
             {t('Главная')}
-            <Input
-                placeholder = 'Введите текст'
-                value={value}
-                onChange={onChange}
-            />
         </div>
     );
 };
