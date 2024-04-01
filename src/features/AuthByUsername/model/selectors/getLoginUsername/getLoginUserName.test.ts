c465d6fa3174a5da61300004e0abcf8e23567f7b
ArizaTrizniak <arizatrizniak@gmail.com>
2024-04-01 15:48:48 -0500

    add unit tests for login form components

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUserName.test.ts
@@ -0,0 +1,18 @@
+import {DeepPartial} from '@reduxjs/toolkit';
+import {StateSchema} from 'app/providers/StoreProvider';
+import {getLoginUsername} from './getLoginUserName';
+
+describe('getLoginUserName.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: '12345'
+            }
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('12345');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
