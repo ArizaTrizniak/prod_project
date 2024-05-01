1a41a31584d8442cfd50972ba091576aaf71b65f
ArizaTrizniak <arizatrizniak@gmail.com>
2024-05-01 08:55:46 -0500

    fix sidebar

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -4,8 +4,9 @@ import React, {memo, useState} from 'react';
 import {ThemeSwitcher} from 'widgets/ThemeSwitcher';
 import LangSwitcher from 'widgets/LangSwitcher/ui/LangSwitcher';
 import Button, {ButtonSize, ButtonTheme} from 'shared/ui/Button/Button';
-import {SidebarItemsList} from '../model/Items';
 import SidebarItem from './SidebarItem';
+import {useSelector} from 'react-redux';
+import {getSidebarItems} from '../model/selectors/getSidebarItems';
 
 interface SidebarProps {
     className?: string;
@@ -13,6 +14,7 @@ interface SidebarProps {
 
 const Sidebar = memo(({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const sidebarItemsList = useSelector(getSidebarItems);
     const onToggle = () => {
         setCollapsed(prev => !prev);
     };
@@ -35,7 +37,7 @@ const Sidebar = memo(({className}: SidebarProps) => {
                 {collapsed ? '>' : '<'}
             </Button>
             <div className={cls.items}>
-                {SidebarItemsList.map((item) => (
+                {sidebarItemsList.map((item) => (
                     <SidebarItem
                         item={item}
                         collapsed={collapsed}
